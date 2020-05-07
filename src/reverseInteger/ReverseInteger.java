d9309de455ab809ff75a7fc1bcded466e58b003a
sk <tranquocsang19977@gmail.com>
2020-05-07 07:55:27 -0700

    fix rev int

--- src/reverseInteger/ReverseInteger.java
+++ src/reverseInteger/ReverseInteger.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class ReverseInteger {
     public static void main(String[] args) {
         int number;
-        Scanner scanner = new Scanner(System.in);
-        number = scanner.nextInt();
+        Scanner u = new Scanner(System.in);
+        number = u.nextInt();
         int reversedNumber = 0;
         int temp = 0;
-        while (number > 2147483647 || number < -2147483647){
+        while (number != 0) {
             temp = number % 10;
+            if (reversedNumber > 2147483647 / 10 || (reversedNumber == 2147483647 / 10 && temp > 7)) {
+                reversedNumber = 0;
+                break;
+            }
+            if (reversedNumber < -2147483647 / 10 || (reversedNumber == -2147483647 / 10 && temp < -8)) {
+                reversedNumber = 0;
+                break;
+            }
             reversedNumber = reversedNumber * 10 + temp;
-            number = number / 10;}
-        if (reversedNumber > 2147483647 || reversedNumber < -2147483647) {
-            System.out.println("0");
-        } else {
-
-            System.out.println(reversedNumber);
+            number = number / 10;
         }
+        System.out.println(reversedNumber);
+
     }
 }
