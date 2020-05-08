8504387c5bf27db8d506b346c74fc98b245a3b76
sk <tranquocsang19977@gmail.com>
2020-05-07 18:36:57 -0700

    fix rev words

--- src/reverseWords/ReverseWords.java
+++ src/reverseWords/ReverseWords.java
@@ -10,9 +10,13 @@ public class ReverseWords {
         String s;
         Scanner scanner=new Scanner(System.in);
         s=scanner.nextLine();
-        List<String> token = Arrays.asList(s.trim().split("\\s+"));
-        Collections.reverse(token);
-        System.out.println(token);
+        String s1[] = s.split(" ");
+        String ans = "";
+        for (int i = s1.length - 1; i >= 0; i--) {
+            ans += s1[i] + " ";
+        }
+        System.out.println("Reversed String:");
+        System.out.println(ans.substring(0, ans.length() - 1));
     }
 
 }
